8a051230b5f0b1d5e3725733b45a3c586abc2d72
DmVa <dmva@ua.fm>
2019-03-27 18:37:00 +0200

    temps

---
+++ QnAppCalendar/components/scheduler/scheduler.customize/schedulercustomize.component.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('scheduler.module')
+        .component('pqSchedulerCustomize', {
+            templateUrl: '/components/scheduler/scheduler.customize/schedulercustomize.component.html',
+            controller: SchedulerCustomizeController
+        });
+
+    SchedulerCustomizeController.inject = ['$scope', '$window', '$document', '$timeout', 'schedulerDataService' ];
+
+    function SchedulerCustomizeController($scope, $window, $document, $timeout, schedulerDataService) {
+        let $ctrl = this;
+      
+    }
+}
+)();
