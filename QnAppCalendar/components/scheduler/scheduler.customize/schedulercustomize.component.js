38b68a2895145eccc0d681edf5f5aa3b47a736fb
DmVa <dmva@ua.fm>
2019-03-27 20:08:23 +0200

    test

--- QnAppCalendar/components/scheduler/scheduler.customize/schedulercustomize.component.js
+++ QnAppCalendar/components/scheduler/scheduler.customize/schedulercustomize.component.js
@@ -12,7 +12,17 @@
 
     function SchedulerCustomizeController($scope, $window, $document, $timeout, schedulerDataService) {
         let $ctrl = this;
-      
+        let stages = [];
+        let statuses = [];
+        let stage = new Stage(1, 'init');
+        stages.push(stage);
+        let stage2 = new Stage(2, 'complete');
+        stages.push(stage2);
+
+        statuses.push(new Status(1, 'In Quees', 1));
+        statuses.push(new Status(2, 'Walk', 1));
+        $scope.stages = stages;
+        $scope.statuses = statuses;
     }
 }
 )();
---
+++ QnAppCalendar/components/scheduler/scheduler.customize/stage.js
@@ -0,0 +1,14 @@
+﻿class Stage {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Status {
+    constructor(id, name, isQflow) {
+        this.id = id;
+        this.name = name;
+        this.isQflow = isQflow;
+    }
+}
